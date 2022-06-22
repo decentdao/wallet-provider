303799c98c5b1ac9aea18854086f07effd1772b1
David Colon <dcolonantonio@gmail.com>
2022-06-22 11:39:23 -0400

    typed export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Web3Provider } from './Web3Provider';
 export { useWeb3Provider } from './hooks/useWeb3Provider';
-export { InitialState, } from './types';
+export type { InitialState } from './types';
